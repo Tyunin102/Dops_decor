a6bfb49d59843d14fcb2dead77a8749eb7e6a373
Pavel <90606526+Tyunin102@users.noreply.github.com>
2024-03-28 14:41:46 +0300

    NoSort

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ def memory(max_size):
 
     def wrapper(func):
         def inner(*args, **kwargs):
-            args_str = ','.join(map(str, sorted(args)))
-            sorted_kwargs = sorted(kwargs.items())
-            kwargs_str = ','.join('{}={}'.format(key, value) for key, value in sorted_kwargs)
+            args_str = ','.join(map(str, args))
+            kwargs_str = ','.join('{}={}'.format(key, value) for key, value in kwargs.items())
             key = '{}|{}'.format(args_str, kwargs_str)
             print(f"{key} is a key")
             if key in cache:
@@ -24,13 +23,12 @@ def memory(max_size):
 @memory(max_size=int(input()))
 def add(a, b):
 
-    return a + b
+    return int(a) + int(b)
 
+print(add(1,2))
+print(add(1,'2'))
+print(add(2,'1'))
 
-print(add(1, 2))
-print(add(2, 1))
-
-print(add(a=1,b=2))
-print(add(b=2,a=1))
-print(add(3,4))
 print(add(a=4,b=3))
+print(add(a='4', b=3))
+print(add(a='4',b='3'))
